078d9b53d7182f874d5685ab56078d470033d9f6
Akino <93914105+CirnoDayo@users.noreply.github.com>
2024-08-25 04:03:07 +0700

    Day 3 Commit #4

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public static int emailsCount = 0;
-    public static bool bossTranscript = false;
+    public static bool meetingDone = false;
 
     private void Awake()
     {
--- Assets/Scripts/MeetingScript.cs
+++ Assets/Scripts/MeetingScript.cs
@@ -1,18 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MeetingScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public LevelLoader LevelLoader;
+    public SpriteRenderer bubbleOne;
+    public SpriteRenderer bubbleTwo;
+    public SpriteRenderer bubbleThree;
+    public Toggle[] correctAnswer = new Toggle[3];
+    public Toggle[] answers = new Toggle[4];
+    public GameObject[] dotdotdot = new GameObject[2];
+    public SpriteRenderer[] correctSprites = new SpriteRenderer[3];
+
+    int corrects = 0;
+    Toggle lastAnswer;
+
+    private void Start()
     {
-        
+        bubbleTwo.color = Color.gray;
+        bubbleThree.color = Color.gray;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateChoice(Toggle Choice)
     {
-        
+        if (Choice != correctAnswer[0] && corrects == 0)
+        {
+            Debug.Log("y");
+            Choice.interactable = false;
+        }
+
+        if (Choice != correctAnswer[1] && corrects == 1)
+        {
+            Debug.Log("y");
+            Choice.interactable = false;
+        }
+
+        if (Choice != correctAnswer[2] && corrects == 2)
+        {
+            Debug.Log("y");
+            Choice.interactable = false;
+        }
+
+        if (Choice == correctAnswer[0] && corrects == 0)
+        {
+            Debug.Log("yy");
+            corrects++;
+            for(int i = 0; i < answers.Length; i++)
+            {
+                answers[i].interactable = true;
+            }
+        }
+
+        if (Choice == correctAnswer[1] && corrects == 1)
+        {
+            Debug.Log("yy");
+            corrects++;
+            for (int i = 0;i < answers.Length;i++)
+            {
+                answers[i].interactable = true;
+            }
+        }
+
+        if (Choice == correctAnswer[2] && corrects == 2)
+        {
+            Debug.Log("yyy");
+            GameManager.meetingDone = true;
+            LevelLoader.LoadSceneWithValue(2);
+        }
+
+        if (corrects == 1)
+        {
+            correctSprites[0].enabled = false;
+            bubbleOne.color = Color.gray;
+            bubbleTwo.color = Color.white;
+            dotdotdot[0].SetActive(false);
+            correctSprites[1].enabled = true;
+        }
+
+        if(corrects == 2)
+        {
+            correctSprites[1].enabled = false;
+            bubbleTwo.color = Color.gray;
+            bubbleThree.color = Color.white;
+            dotdotdot[1].SetActive(false);
+            correctSprites[2].enabled = true;
+        }
     }
 }
